16ce31ba69e3ec4bfcee057be94019115206c331
Natalya <hristNBshca@yandex.ru>
2022-02-11 13:14:15 +0300

    create: validation

--- js/main.js
+++ js/main.js
@@ -45,7 +45,23 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
-
-
-
+  // Обработка форм
+  $('.form').each(function() {
+    $(this).validate({
+        errorClass: "invalid",
+        messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name must be at least 2 letters long",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "We need your phone nambe to contact you"
+        }
+      },
+    });
+  })
 });
